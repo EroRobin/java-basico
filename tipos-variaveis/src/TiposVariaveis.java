a43b79a49b27e0df770dd172a5366ad0757904b6
EroRobin <Robinson.rubens.3@gmail.com>
2024-02-26 15:07:09 -0300

    Criando a classe TiposVariaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,24 @@
+public class TiposVariaveis {
+    public static void main (String[] args) {
+        byte tipoByte = 127;
+        short tipoShort = 32767;
+        char tipoChar = 'C';
+        float tipoFloat = 2.6f;
+        double tipoDouble = 3.59;
+        int tipoInt = 2147483647;
+        long tipoLong = 9223372036854775807L;
+        boolean tipoBooleano = true;
+        System.out.println("Valor do tipoByte = " + tipoByte);
+        System.out.println("Valor do tipoShort = " + tipoShort);
+        System.out.println("Valor do tipoChar = " + tipoChar);
+        System.out.println("Valor do tipoFloat = " + tipoFloat);
+        System.out.println("Valor do tipoDouble = " + tipoDouble);
+        System.out.println("Valor do tipoInt = " + tipoInt);
+        System.out.println("Valor do tipoLong = " + tipoLong);
+        System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+
+        //Para garantir que o valor não seja alterado, coloque a expressão (final)
+        // final int numero = 5;
+        //    System.out.println(numero);
+    }
+}
