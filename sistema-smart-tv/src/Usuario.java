9023228e965dd3c84d34a78c45548e16d65d18cc
EroRobin <Robinson.rubens.3@gmail.com>
2024-02-26 15:19:06 -0300

    Criando a classe SmartTV e suas funcionalidades

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,5 +1,27 @@
 public class Usuario {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SmartTV smartTV = new SmartTV();
+
+        System.out.println("Canal atual: " + smartTV.canal);
+
+        smartTV.mudarCanal(26);
+
+        System.out.println("Volume atual: " + smartTV.volume);
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+        System.out.println("Volume atual: " + smartTV.volume);
+
+        System.out.println("TV ligada? " + smartTV.ligada);
+        System.out.println("TV qual canal está: " + smartTV.canal);
+        System.out.println("TV que volume está: " + smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("TV ligada? " + smartTV.ligada);
+
+        smartTV.desligar();
+        System.out.println("TV ligada? " + smartTV.ligada);
     }
 }
