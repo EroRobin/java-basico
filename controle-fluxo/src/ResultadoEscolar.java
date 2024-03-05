d92f357ba12759d5ee18464fa5474f9d017f8fa6
EroRobin <Robinson.rubens.3@gmail.com>
2024-03-05 09:39:16 -0300

    Adicionando um exemplo de condição ternátia

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -9,5 +9,10 @@ public class ResultadoEscolar {
 
         else
             System.out.println("Reprovado!");
+
+        //Usando condição ternária
+
+        String resultado = nota>7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação":"Reprovado";
+        System.out.println(resultado);
     }
 }
