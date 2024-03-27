f0ed349697058b9e2beb663ce831ff8a56a88274
EroRobin <Robinson.rubens.3@gmail.com>
2024-03-27 15:27:42 -0300

    criando exemplos da utilização de pacotes para melhor organização

---
+++ POO/exemplo-pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,13 @@
+package com.mastersoft.clinical.service;
+
+//importando uma classe de outro pacote
+import com.mastersoft.clinical.model.Exame;
+
+//Classe para realizar operações dos exames (marcar exame, consultar exame...)
+public class ExameService {
+    //método para salvar exame
+    void salvarExame(Exame exame){
+
+    }
+
+}
