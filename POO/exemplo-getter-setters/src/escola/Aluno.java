bae1fef1e85b1ecee8caa9d2bf5055ca6ea36aae
EroRobin <Robinson.rubens.3@gmail.com>
2024-04-01 09:27:28 -0300

    colocando alguns comentarios

--- POO/exemplo-getter-setters/src/escola/Aluno.java
+++ POO/exemplo-getter-setters/src/escola/Aluno.java
@@ -4,6 +4,8 @@ public class Aluno {
    private String nome;
    private int idade;
 
+   //o método Getter retorna o valor do atributo especificado
+   // o método Setter define outro novo valor para o atributo especificado
    public String getNome(){
        return nome;
    }
