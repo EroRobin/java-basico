7a5daa730039c97dae70bad0ab9c62a69cb85a8b
EroRobin <Robinson.rubens.3@gmail.com>
2024-04-05 09:40:21 -0300

    Criando as classes 'Impressora', 'Copiadora' e 'Digitalizadora' que irao representar os equipamentos com suas finalidades especificas

--- POO/exemplo-inteface/src/Main.java
+++ POO/exemplo-inteface/src/Main.java
@@ -1,3 +1,4 @@
+//Interface (Conceito de abstração) é a capacidade de um mesmo objeto ter propriedades e papeis diferentes em uma aplicação
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
